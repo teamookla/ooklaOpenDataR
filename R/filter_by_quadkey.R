74977acc5f0f61ffeab708d39bc05ef35ae3af7c
Katie Jolly <katie.jolly@ookla.com>
2020-10-02 15:56:07 -0700

    check that sf is installed

--- R/filter_by_quadkey.R
+++ R/filter_by_quadkey.R
@@ -38,7 +38,7 @@ tileXYToQuadKey <- function(xTile, yTile, z) {
 #' Filter Performance Tiles by Quadkey
 #'
 #' @param tiles Tiles to filter, from `get_performance_tiles()`
-#' @param bbox Bounding box to pull tiles from, from `st_bbox()`. This way you can just get the specific tiles that you need for an analysis
+#' @param bbox Bounding box to pull tiles from (crs = 4326), from `st_bbox()`. This way you can just get the specific tiles that you need for an analysis.
 #'
 #' @return Filtered tiles with the same class as the `tiles` input, either data frame or `sf`
 
@@ -47,7 +47,10 @@ filter_by_quadkey <- function(tiles, bbox) {
   assertthat::assert_that(inherits(bbox, "bbox"))
 
   # make sure the coordinates are lat/lon
-  bbox = sf::st_bbox(sf::st_transform(sf::st_as_sfc(bbox), 4326))
+  if (rlang::is_installed("sf")) {
+    bbox = sf::st_bbox(sf::st_transform(sf::st_as_sfc(bbox), 4326))
+  }
+
   tile_grid <- slippymath::bbox_to_tile_grid(bbox, zoom = 16)
 
   quadkeys <- mapply(tileXYToQuadKey, xTile = tile_grid$tiles$x, yTile = tile_grid$tiles$y, MoreArgs = list(z = 16))
